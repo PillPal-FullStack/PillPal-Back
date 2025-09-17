c63fb44c189fc3d4c42770ce32c569e5b182f959
mary kenny <marykenny2704@yahoo.com>
2025-09-17 15:00:07 +0200

    UserDetail,JwtAuthFilter,JwtService): add UserDetail and jwt

---
+++ src/main/java/com/project/pillpal/user/security/UserDetail.java
@@ -0,0 +1,35 @@
+package com.project.pillpal.user.security;
+
+import com.project.pillpal.user.entity.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Getter
+public class UserDetail implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+}
