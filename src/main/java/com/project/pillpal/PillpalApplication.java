e29756aa2f483c0195c8739aed6ef8885fa1569e
tizzifona <tizzifona@gmail.com>
2025-09-17 11:48:11 +0200

    chore: setup project

---
+++ src/main/java/com/project/pillpal/PillpalApplication.java
@@ -0,0 +1,13 @@
+package com.project.pillpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillpalApplication.class, args);
+	}
+
+}
