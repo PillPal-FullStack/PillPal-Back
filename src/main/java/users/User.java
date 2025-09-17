4493850a6d3fe101177a2257f7d9147a82b74fa9
mary kenny <marykenny2704@yahoo.com>
2025-09-17 13:38:14 +0200

    feat(User, UserRepository, UserRequest): add user model, repository and begin dto for user

---
+++ src/main/java/users/User.java
@@ -0,0 +1,44 @@
+package users;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<Medication> challenges = new ArrayList<>();
+
+
+
+
+
+
+}
