87f2ca432bfb3c0123830342b3bd181bd096ed6f
mary kenny <marykenny2704@yahoo.com>
2025-09-17 16:04:06 +0200

    UserRepository,UserService(add methods to UserRepository, add registerUser and loddByUsername to UserService)

---
+++ src/main/java/com/project/pillpal/user/service/UserService.java
@@ -0,0 +1,53 @@
+package com.project.pillpal.user.service;
+
+import com.project.pillpal.user.dtos.UserMapperImpl;
+import com.project.pillpal.user.dtos.UserRequest;
+import com.project.pillpal.user.dtos.UserResponse;
+import com.project.pillpal.user.entity.Role;
+import com.project.pillpal.user.entity.User;
+import com.project.pillpal.user.repository.UserRepository;
+import com.project.pillpal.user.security.UserDetail;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final UserMapperImpl userMapperImpl;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetail loadUserByUsername(String username) throws EntityNotFoundException {
+        User user = userRepository.findByUsernameIgnoreCase(username)
+                .orElseThrow(() -> new EntityNotFoundException(User.class.getSimpleName(), username));
+        return new UserDetail(user);
+    }
+
+    @Transactional
+    public UserResponse registerUser(UserRequest request) {
+        if (userRepository.existsByUsername(request.username())) {
+            throw new IllegalArgumentException("Username is already taken.");
+        }
+
+        if (userRepository.existsByEmail(request.email())) {
+            throw new IllegalArgumentException("Email is already registered");
+        }
+
+        Role requestedRole = request.role();
+
+        User user = userMapperImpl.dtoToEntity(request);
+
+        user.setPassword(passwordEncoder.encode(request.password()));
+
+        userRepository.save(user);
+        return userMapperImpl.entityToDto(user);
+    }
+}
